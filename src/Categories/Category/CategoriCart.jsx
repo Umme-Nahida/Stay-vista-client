4f8d525caf76a873e23a0aa32155bfbd340ad7b2
Umme-Nahida <mysuccessmission@gmail.com>
2024-08-22 18:54:02 +0600

    install query string

--- src/Categories/Category/CategoriCart.jsx
+++ src/Categories/Category/CategoriCart.jsx
@@ -1,9 +1,31 @@
 /* eslint-disable react/prop-types */
 
+import queryString from "query-string";
+import { useNavigate, useSearchParams } from "react-router-dom";
+
 const CategoriCart = ({title,icon:Icon}) => {
+    const [params,setParams]=useSearchParams()
+    const navigate=useNavigate()
+    console.log(params.toString())
+    
+    const handleCategory=()=>{
+        console.log('clicked',title)
+        let currentQuery = {};
+        if(params){
+            currentQuery=queryString.parse(params.toString())
+        }
+        const updateQuery = {...currentQuery, category:title}
+            console.log(updateQuery)
+        const url = queryString.stringifyUrl({
+            url:'/',
+            query:updateQuery
+        })
+        console.log(url)
+        navigate(url);
+    }
 
     return (
-        <div className="flex flex-col items-center justify-center gap-1 border-b-2 text-slate-500 hover:text-neutral-800 transition cursor-pointer">
+        <div onClick={handleCategory} className="flex flex-col items-center justify-center gap-1 border-b-2 text-slate-500 hover:text-neutral-800 transition cursor-pointer">
             <Icon className='text-xl'></Icon>
             <div className="text-lg">
                 {title}
