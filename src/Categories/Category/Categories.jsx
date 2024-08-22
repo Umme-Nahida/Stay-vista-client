d2272ae0c025110a657a7d1800dbc53c86aef88b
Umme-Nahida <mysuccessmission@gmail.com>
2024-08-23 02:29:53 +0600

    some change in category page

--- src/Categories/Category/Categories.jsx
+++ src/Categories/Category/Categories.jsx
@@ -1,15 +1,21 @@
+import { useSearchParams } from "react-router-dom";
 import Container from "../../components/Shared/Container";
 import { categories } from "../CategoriesData";
 import CategoriCart from "./CategoriCart";
 
 const Categories = () => {
-
+    const [params,setParams]=useSearchParams()
+    console.log(params)
+    const category = params.get('category')
+    console.log(category)
     return (
        <Container>
          <div className="flex items-center justify-between gap-5 overflow-x-auto mb-10">
             {
                 categories.map(item=>(
-                    <CategoriCart key={item.label} title={item.label} icon={item.icon}></CategoriCart>
+                    <CategoriCart key={item.label}  title={item.label} icon={item.icon} 
+                    selectedCategory={category === item.label}
+                    ></CategoriCart>
                 ))
             }
         </div>
