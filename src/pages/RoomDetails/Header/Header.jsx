f1209a1c88d4f4a05e1a07d718ffe435b0eb45f2
Umme-Nahida <mysuccessmission@gmail.com>
2024-08-25 21:26:00 +0600

    adde heading hooks

--- src/pages/RoomDetails/Header/Header.jsx
+++ src/pages/RoomDetails/Header/Header.jsx
@@ -11,9 +11,9 @@ const Header = ({ room }) => {
         <>
         
           <Heading title={room?.title} subtitle={room?.location} />
-          <div className="w-full md:h-[60vh] overflow-hidden rounded-xl">
+          <div className="w-full md:h-[60vh] rounded-xl">
             <img
-              className="object-cover w-full"
+              className="object-cover h-full w-full"
               src={room?.image}
               alt="header image"
             />
