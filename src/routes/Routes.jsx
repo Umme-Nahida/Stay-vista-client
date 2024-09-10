6d327cfdd1c08b9a2581274811e51ef58781b1a2
Umme-Nahida <mysuccessmission@gmail.com>
2024-09-10 18:01:00 +0600

    create dashboard router

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -5,6 +5,7 @@ import ErrorPage from '../pages/ErrorPage'
 import Login from '../pages/Login/Login'
 import SignUp from '../pages/SignUp/SignUp'
 import RoomDetails from '../pages/RoomDetails/RoomDetails'
+import DashboardLayout from '../components/Dashboard/DashboardLayout'
 
 export const router = createBrowserRouter([
   {
@@ -25,4 +26,9 @@ export const router = createBrowserRouter([
   },
   { path: '/login', element: <Login /> },
   { path: '/signup', element: <SignUp /> },
+  { path: '/dashboard', element: <DashboardLayout></DashboardLayout>,
+    children:[
+      
+    ]
+  },
 ])
