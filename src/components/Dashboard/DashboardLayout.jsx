b59a7ab7d078492282cea33a8ffe527332d8d8db
Umme-Nahida <mysuccessmission@gmail.com>
2024-09-11 23:47:38 +0600

    added calender date range

--- src/components/Dashboard/DashboardLayout.jsx
+++ src/components/Dashboard/DashboardLayout.jsx
@@ -7,7 +7,7 @@ const DashboardLayout = () => {
         <div className='relative md:flex min-h-screen'>
                 {/* sidebar */}
                  <Sidebar></Sidebar>
-            <div className='flex-1 p-4 md:ml-64 bg-red-500'>
+            <div className='flex-1 p-4 md:ml-64'>
                 {/* outlet */}
                <Outlet></Outlet>
             </div>
