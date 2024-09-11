6117c9459bcd9f39b3a5ae98eb963fa80b06b977
Umme-Nahida <mysuccessmission@gmail.com>
2024-09-12 02:03:56 +0600

    create api for save room data

--- src/api/rooms.js
+++ src/api/rooms.js
@@ -1,7 +1,13 @@
 import axios from "axios"
 
-
+// get all rooms data
 export const getAllRooms = async()=>{
     const {data} = await axios.get('http://localhost:8000/rooms')
+    return data
+}
+
+// save rooms data in db
+export const saveRooms = async(roomsData)=>{
+    const {data} = await axios.post('http://localhost:8000/saveRooms',roomsData,{withCredentials:true})
     return data
 }
