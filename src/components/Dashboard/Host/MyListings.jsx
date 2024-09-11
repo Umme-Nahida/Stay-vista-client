1178a9fb25cee2be04b1c63869d655b7f6e4fd21
Umme-Nahida <mysuccessmission@gmail.com>
2024-09-11 23:46:19 +0600

    add react helmet

--- src/components/Dashboard/Host/MyListings.jsx
+++ src/components/Dashboard/Host/MyListings.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from "react";
+import { Helmet } from "react-helmet-async";
 
 const MyListings = () => {
-    return (
-        <div className='bg-blue-500'>
-           <h1>my listings</h1>
-        </div>
-    );
+  return (
+    <div className="bg-blue-500">
+      <Helmet>
+        <title>My Listings | Dashboard</title>
+      </Helmet>
+      <h1>my listings</h1>
+    </div>
+  );
 };
 
 export default MyListings;
