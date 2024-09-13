c6f05051a35ee515608342d2e035b3b18333ed1b
Umme-Nahida <mysuccessmission@gmail.com>
2024-09-13 20:59:06 +0600

    create function for save user to the db

--- src/api/rooms.js
+++ src/api/rooms.js
@@ -1,9 +1,16 @@
 import axios from "axios"
+import axiosSecure from "."
 
 // get all rooms data
 export const getAllRooms = async()=>{
-    const {data} = await axios.get('http://localhost:8000/rooms')
+    const {data} = await axiosSecure.get('/rooms')
     return data
+}
+
+// get single value
+export const getRoom = async id=>{
+    const {data} = await axiosSecure.get(`/rooms/${id}`)
+    return data 
 }
 
 // save rooms data in db
@@ -11,3 +18,27 @@ export const saveRooms = async(roomsData)=>{
     const {data} = await axios.post('http://localhost:8000/saveRooms',roomsData,{withCredentials:true})
     return data
 }
+
+// get host rooms by host email 
+export const hostRooms = async email =>{
+    const {data}= await axiosSecure.get(`/hostRooms/${email}`)
+    return data 
+}
+
+// save user to the database
+export const saveUser=async user=>{
+
+    const userInfo = {
+        email: user?.email,
+        role:'admin',
+        status: "Verified"
+    }
+   const {data}= await axiosSecure.put(`/users/${user?.email}`,userInfo)
+   return data;
+}
+
+export const clearCookie = async()=>{
+    const {data}= await axiosSecure.post('/logout')
+    return data 
+}
+
