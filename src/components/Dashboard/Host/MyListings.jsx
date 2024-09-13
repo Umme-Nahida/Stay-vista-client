8ac9361855bb116c947d95e831f4baf4b059389b
Umme-Nahida <mysuccessmission@gmail.com>
2024-09-13 21:00:44 +0600

    my listings data includes

--- src/components/Dashboard/Host/MyListings.jsx
+++ src/components/Dashboard/Host/MyListings.jsx
@@ -1,13 +1,94 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Helmet } from "react-helmet-async";
+import useAuth from "../../../hooks/useAuth";
+import { hostRooms } from "../../../api/Rooms";
+import RoomDataRow from "../TableRow/RoomDataRow";
 
 const MyListings = () => {
+  const {user}= useAuth()
+  console.log(user)
+  const [listing,setListing] = useState([])
+
+  useEffect(()=>{
+    
+    hostRooms(user?.email)
+    .then(data=>{
+      setListing(data)
+    })
+  },[user?.email])
+  console.log(listing)
   return (
-    <div className="bg-blue-500">
+    <div className="">
       <Helmet>
         <title>My Listings | Dashboard</title>
       </Helmet>
-      <h1>my listings</h1>
+      <div className='container mx-auto px-4 sm:px-8'>
+        <div className='py-8'>
+          <div className='-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto'>
+            <div className='inline-block min-w-full shadow rounded-lg overflow-hidden'>
+              <table className='min-w-full leading-normal'>
+                <thead>
+                  <tr>
+                    <th
+                      scope='col'
+                      className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
+                    >
+                      Title
+                    </th>
+                    <th
+                      scope='col'
+                      className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
+                    >
+                      Location
+                    </th>
+                    <th
+                      scope='col'
+                      className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
+                    >
+                      Price
+                    </th>
+                    <th
+                      scope='col'
+                      className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
+                    >
+                      From
+                    </th>
+                    <th
+                      scope='col'
+                      className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
+                    >
+                      To
+                    </th>
+                    <th
+                      scope='col'
+                      className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
+                    >
+                      Delete
+                    </th>
+                    <th
+                      scope='col'
+                      className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
+                    >
+                      Update
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {/* Room row data */}
+                  {
+                    listing.map(item=>(
+                      <RoomDataRow
+                      key={item._id}
+                      room={item}
+                      ></RoomDataRow>
+                    ))
+                  }
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
